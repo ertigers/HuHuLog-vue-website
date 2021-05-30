33cc09f24ea7a6a36032aabc7f29dc28cf9ec88a
yihu-lv <1720427782@qq.com>
2021-05-30 16:23:07 +0800

    update

---
+++ precache-manifest.8b3c797f08108c6fc8743eb6ed2578a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5469eff5589a4b49d2d8",
+    "url": "/HuHuLog-vue-website/css/app.8bca4d01.css"
+  },
+  {
+    "revision": "031eeb087a34744d953b351d21c14102",
+    "url": "/HuHuLog-vue-website/index.html"
+  },
+  {
+    "revision": "5469eff5589a4b49d2d8",
+    "url": "/HuHuLog-vue-website/js/app.fb26c3f2.js"
+  },
+  {
+    "revision": "1aa2c622dadce109e436",
+    "url": "/HuHuLog-vue-website/js/chunk-vendors.e896cd8f.js"
+  },
+  {
+    "revision": "ef4496c4a9eb1c57a4fcb179c6e952ad",
+    "url": "/HuHuLog-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/HuHuLog-vue-website/robots.txt"
+  }
+]);
